8e65310b2b8100209367dc23c5dfb77c6219d41c
FranciscoPedrezzetti <panchopedre@gmail.com>
2023-01-04 21:17:40 -0300

    ya me levanta inicio :D

--- Blog_FP/urls.py
+++ Blog_FP/urls.py
@@ -21,4 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/', include('users.urls')),
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,16 @@ app_name = 'blog'
 urlpatterns = [
     # Inicio
     path('', inicio, name='Inicio'),
-  
+    # Posts
+    path('pages/', posts, name='Posts'),
+    path('pages/new_post/', agregar_post, name='NewPost'),
+    path('pages/delete_post/<int:pk>', DeletePost.as_view(), name='DeletePost'),
+    path('pages/edit_post/<post_id>/', edit_post, name='EditPost'),
+    path('pages/<post_id>/', post_detail, name='PostDetail'),
+    # About
+    path('about/', about, name='About'),
+    # Messages
+    path('messages/', messages, name='Messages'),
+    path('messages/delete/<msg_id>/', delete_msg, name='DeleteMsg'),
+    path('messages/new_msg/', new_message, name='NewMsg'),
 ]
